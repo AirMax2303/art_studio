51f19dfbafdba2fb022920145d300fa8bb3123b1
LysenkoWork <lyss.work@gmail.com>
2023-01-08 17:49:30 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/art_studio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.art_studio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'art_model.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+      //  title: Text(''),
+      //),
+      body: GridView.count(
+        crossAxisCount: 4,
+        children: List.generate(arts.length, (index) {
+          return Text(arts[index].author);
+        }),
+      ),
+    );
+  }
+}
